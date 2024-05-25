71bb17cfd8746c00301da5b4a253ade2574a733f
jamesferry84 <jamesferry84@hotmail.com>
2024-05-25 16:33:03 +0100

    project setup

---
+++ Assets/Scripts/WaveConfigSO.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Wave Config", fileName = "New Wave Config")]
+public class WaveConfigSO : ScriptableObject
+{
+   [SerializeField] private Transform pathPrefab;
+   [SerializeField] private float moveSpeed = 5f;
+
+   public float GetMoveSpeed()
+   {
+      return moveSpeed;
+   }
+
+   public Transform GetStartingWaypoint()
+   {
+      return pathPrefab.GetChild(0);
+   }
+
+   public List<Transform> GetWaypoints()
+   {
+      List<Transform> waypoints = new List<Transform>();
+      foreach (Transform child in pathPrefab)
+      {
+         waypoints.Add(child);
+      }
+
+      return waypoints;
+   }
+}
